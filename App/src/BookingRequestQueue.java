a227de7743c64fee7268aab167ea3631fa374890
bibekSpaceX <bibekkathariya1000@gmail.com>
2026-03-24 15:08:22 +0530

    UC5 Completed

---
+++ App/src/BookingRequestQueue.java
@@ -0,0 +1,22 @@
+import java.util.*;
+
+public class BookingRequestQueue {
+
+    private Queue<Reservation> requestQueue;
+
+    public BookingRequestQueue() {
+        requestQueue = new LinkedList<>();
+    }
+
+    public void addRequest(Reservation reservation) {
+        requestQueue.offer(reservation);
+    }
+
+    public Reservation getNextRequest() {
+        return requestQueue.poll();
+    }
+
+    public boolean hasPendingRequests() {
+        return !requestQueue.isEmpty();
+    }
+}
